c14d5520edd82e16b6c750f24c0d421682bc45cb
Pratyush Rahul Ranjan <ranjan.pratyush24@gmail.com>
2025-11-27 00:17:13 +0530

    lol

--- Express/index.js
+++ Express/index.js
@@ -1,24 +1,16 @@
 import express from 'express'
+import { userLogin, userSignup } from './controller.js'
 
 const app = express()
 
 const PORT = 3000
 
-//Define a simple route
 app.get('/', (req, res)=>{
     res.send('Hello, Express')
 })
 
-app.get('/user/:username', (req, res) => {
-    const username = req.params.username;
-    res.send(`Welcome ${username}`)
-})
-
-//search?keyword=express
-app.get('/search', (req, res)=>{
-    const keyword = req.query.keyword;
-    res.send(`Searching for ${keyword}`)
-})
+app.get('/user/login', userLogin)
+app.get('/user/signup', userSignup)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
