d2382a23782a0025e51eeee3ca621dbebf95b970
Pratyush Rahul Ranjan <ranjan.pratyush24@gmail.com>
2025-11-30 22:02:50 +0530

    lol

--- Express/index.js
+++ Express/index.js
@@ -11,6 +11,13 @@ app.get('/', (req, res)=>{
 
 app.use('/user', router)
 
+app.post('/users', (req, res) => {
+    const {name, email} = req.body
+    res.json({
+        message:`User ${name} with email ${email} created successfully`
+    })
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
